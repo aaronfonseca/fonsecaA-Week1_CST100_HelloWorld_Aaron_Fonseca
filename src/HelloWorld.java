f282d9bcd984f7cb94bdc1e1e33829adf6964fab
Aaron Fonseca <aaron.fonseca@gcu.edu>
2016-06-26 13:18:12 -0700

    Aaron Fonseca CST100 HelloWorld Assignment

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/** Program: Hello World
+* File: HelloWorld.java
+* Summary: Install Eclipse and JDK
+* Author: Aaron Fonseca
+* Date: June 24, 2016
+**/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Aaron Fonseca Installed Eclipse and JDK
+		System.out.println("Hello world!");
+	}
+
+}
